6069a6f8ef9cb2e3896f7b20764ab59809ddc651
zyyg <1146921737@qq.com>
2024-05-23 21:51:18 +0800

    1

---
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -0,0 +1,15 @@
+package com.itheima.reggie;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author yyzhou
+ * @Date 2024/5/23 21:48
+ * @PackageName:com.itheima.reggie
+ * @ClassName: ReggieApplication
+ * @Description: TODO
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class ReggieApplication {
+}
