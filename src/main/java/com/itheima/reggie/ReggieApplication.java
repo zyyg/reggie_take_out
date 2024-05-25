4441b203c316f134632b5f110129fba7cf4763c3
zyyg <1146921737@qq.com>
2024-05-25 15:47:29 +0800

    1

--- src/main/java/com/itheima/reggie/ReggieApplication.java
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -1,5 +1,7 @@
 package com.itheima.reggie;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -10,6 +12,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Description: TODO
  * @Version 1.0
  */
+@Slf4j
 @SpringBootApplication
 public class ReggieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieApplication.class,args);
+        log.info("项目启动成功...");
+    }
 }
---
+++ src/main/java/com/itheima/reggie/config/WebMvcConfig.java
@@ -0,0 +1,29 @@
+package com.itheima.reggie.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * @Author yyzhou
+ * @Date 2024/5/25 10:58
+ * @PackageName:com.itheima.reggie.config
+ * @ClassName: WebMvcConfig
+ * @Description: TODO
+ * @Version 1.0
+ */
+@Slf4j
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+    /**
+     * 静态资源映射
+     * @param registry
+     */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        log.info("开始进行资源映射");
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+}
